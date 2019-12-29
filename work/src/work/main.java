7fe1d79dd057a866dbf2a6fe222e88b6e47ff077
ssabchobo <lch9502@naver.com>
2019-12-29 19:07:11 +0900

    array prac

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,23 +2,18 @@ package work;
 
 import java.util.*;
 
-public class main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int intarray[] = new int[5];
-		System.out.print("5개의 숫자 입력 : ");
-		for (int i=0; i < intarray.length;i++) {
-			intarray[i] = scan.nextInt();
-		}
-		
-		for (int i = 0; i < 5; i++) {
-			System.out.print(intarray[i]+" : ");
-			for (int j = 0; j < intarray[i]; j++) {
-				System.out.print("*");
-			}
-			System.out.print("\n");
-		}
+public class main{
+	public static void main(String[] argc) {
+		// 2차원배열 선언과 동시에 초기화
+        int[][] ar = { { 10, 20 }, { 30, 40 },{50,60} };
+        int i, j;
+        
+        // 행과 열을 출력하기위해 반복문 2
+        for (i = 0; i < ar.length; i++) 
+        {
+            for (j = 0; j < ar[i].length; j++)
+                System.out.print("  " + ar[i][j]);
+            System.out.println();
+        }
 	}
 }
-		
