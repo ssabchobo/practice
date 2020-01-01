dce20f0f3e6952acaf3767a2b1a152bd8d565ab7
ssabchobo <lch9502@naver.com>
2020-01-01 13:45:58 +0900

    생성자

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -3,26 +3,21 @@ package work;
 import java.util.*;
 
 public class main{
+	private String title;
+	
+	
+	public main(String title) {
+		this.title = title;
+	}
+	
+	private String getTitle() {
+		return title;
+	}
+	
 	public static void main(String[] argc) {
-		Name name = new Name("홍길동","임꺽정");
-		System.out.println(name.getMyName()+name.getYourName());
-	}
-}
-
-class Name{
-	String myName;
-	String yourName;
-	public Name(String myName,String yourName) {
-		setName(myName,yourName);
-	}
-	public String getMyName() {
-		return myName;
-	}
-	public String getYourName() {
-		return yourName;
-	}
-	public void setName(String myName,String yourName) {
-		this.myName=myName;
-		this.yourName=yourName;
+		main mySong = new main("Let it go");
+		main yourSong = new main("강남스타일");
+		System.out.println("내 노래는 " + mySong.getTitle());
+		System.out.println("너 노래는 "+ yourSong.getTitle());
 	}
 }
