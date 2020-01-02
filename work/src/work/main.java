2c91c14293793d5c552679154e0f0e6e5687606b
ssabchobo <lch9502@naver.com>
2020-01-02 22:57:35 +0900

    class2

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,27 +2,36 @@ package work;
 
 import java.util.*;
 
+class ArrayUtil {
+	public static int[] concat(int[] a, int[] b) {
+		/* a와 b를 연결한 새로운 배열 리턴 */
+		int arr[] = new int[a.length+b.length];
+		
+		for(int i=0;i<a.length;i++) {
+			arr[i] = a[i];
+		}
+		
+		for(int i =0;i<b.length;i++) {
+			arr[a.length+i] = b[i];
+		}
+		return arr;
+	}
+
+	public static void print(int[] a) {
+		/* 배열 a 출력 */
+		System.out.print("[");
+		for(int n : a) {
+			System.out.print(n+" ");
+		}
+		System.out.print("]");
+		}
+}
+
 public class main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int a;
-		int []arr = new int[10];
-		
-		System.out.println("숫자를 10개 입력하시오.");
-		for(int i=0;i<10;i++) {
-			a = in.nextInt();
-			if(a>=0) {
-				arr[(a)/10]++;
-			}
-		}
-		for(int i=0;i<10;i++) {
-			System.out.printf("%d~%d : ",(i*10),((i+1)*10)-1);
-			for(int j=0;j<arr[i];j++) {
-				
-				System.out.print("*");
-			}
-			System.out.print("\n");
-		}
-		
+	public static void main (String[] args) {
+		int [] array1 = {1, 3, 5, 7, 9};
+		int [] array2 = {2, 4, 6, 8, 10};
+		int [] array3 = ArrayUtil.concat(array1, array2);
+		ArrayUtil.print(array3);
 	}
 }
