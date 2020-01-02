3dff74e7c8af4bc1e6c4dc4990f35bcc0b015943
ssabchobo <lch9502@naver.com>
2020-01-02 16:18:14 +0900

    행렬

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,45 +2,23 @@ package work;
 
 import java.util.*;
 
-public class main{
-	public static void main(String[] argc) {
-		Scanner in = new Scanner(System.in);
-		System.out.print("인원수>> ");
-		int p = in.nextInt();
-		Phone [] r = new Phone[p];
-		for(int i=0;i<p;i++) {
-			System.out.print("이름과 전화번호(번호는 연속적으로 입력)>> ");
-			r[i] = new Phone(in.next(),in.next());
-		}
-		System.out.println("저장되었습니다...");
-		
-		while (true) {
-			System.out.print("검색할 이름>> ");
-			String n = in.next();
-			if(n.equals("exit")) {
-				break;
-			} else {
-				for (int i = 0; i < p; i++) {
-					if (n.equals(r[i].getName())) {
-						System.out.println(n + "의 번호는" + r[i].getTel() + "입니다");
-					}
-				}
+public class main {
+	public static void main(String[] args) {
+		int[][] score = { { 100, 100, 100 }, { 20, 20, 20 }, { 30, 30, 30 }, { 40, 40, 40 }, { 50, 50, 50 } };
+		int[][] result = new int[score.length + 1][score[0].length + 1];
+		for (int i = 0; i < score.length; i++) {
+			for (int j = 0; j < score[i].length; j++) {
+				result[i][j] = score[i][j];
+				result[i][score[0].length] += result[i][j];
+				result[score.length][j] += result[i][j];
+				result[score.length][score[0].length] += result[i][j];
 			}
 		}
-	}
-}
-
-class Phone{
-	private String name, tel;
-	
-	public Phone(String name, String tel) {
-		this.name = name;
-		this.tel = tel;
-	}
-	public String getName() {
-		return name;
-	}
-	public String getTel() {
-		return tel;
+		for (int i = 0; i < result.length; i++) {
+			for (int j = 0; j < result[i].length; j++) {
+				System.out.printf("%4d", result[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
