3818c9ceb4dac8e66fb3d984d46f0f5f1e560539
ssabchobo <lch9502@naver.com>
2020-01-02 17:02:16 +0900

    class2

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -4,21 +4,45 @@ import java.util.*;
 
 public class main {
 	public static void main(String[] args) {
-		int[][] score = { { 100, 100, 100 }, { 20, 20, 20 }, { 30, 30, 30 }, { 40, 40, 40 }, { 50, 50, 50 } };
-		int[][] result = new int[score.length + 1][score[0].length + 1];
-		for (int i = 0; i < score.length; i++) {
-			for (int j = 0; j < score[i].length; j++) {
-				result[i][j] = score[i][j];
-				result[i][score[0].length] += result[i][j];
-				result[score.length][j] += result[i][j];
-				result[score.length][score[0].length] += result[i][j];
+		Scanner in = new Scanner(System.in);
+		Random r = new Random();
+		
+		int low,high;
+		int number;
+		
+		while(true) {
+			int step = 0;
+			low = 0;
+			high = 99;
+			
+			number = r.nextInt(100);
+			
+			System.out.println("수를 결정하였습니다. 맞추어 보세요");
+			
+			while(true) {
+				int input;
+				System.out.println(low + "-" + high);
+				System.out.print(step+1+">>");
+				input = in.nextInt();
+				
+				if(input<number) {
+					System.out.println("더 높게");
+					low = input;
+				}
+				else if(input<number) {
+					System.out.println("더 낮게");
+					high = input;
+				}else {
+					System.out.println("맞았습니다.");
+					break;
+				}
+				step ++;
 			}
-		}
-		for (int i = 0; i < result.length; i++) {
-			for (int j = 0; j < result[i].length; j++) {
-				System.out.printf("%4d", result[i][j]);
+			System.out.print("다시 하시겠습니까(y/n)>>");
+			String answer = in.next();
+			if(answer.contentEquals("n")) {
+				break;
 			}
-			System.out.println();
 		}
 	}
 }
