0c06041fcc3a5113ae70f2d49df5faabdf3315ed
ssabchobo <lch9502@naver.com>
2020-01-02 17:24:22 +0900

    class4

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -4,20 +4,17 @@ import java.util.*;
 
 public class main {
 	public static void main(String[] args) {
-		foo("æ»≥Á", 1);
-		foo("æ»≥Á«œººø‰", 1, 2);
-		foo("¿ﬂ ¿÷æÓ");
-	}
-	
-	public static void foo(String a, int b) {
-		System.out.println(a+" "+b);
-	}
-	
-	public static void foo(String a, int b, int c) {
-		System.out.println(a+" "+b+" "+c);
-	}
-	
-	public static void foo(String a) {
-		System.out.println(a);
+		int result =0;
+		int i=0;
+		
+		while(result<100) {
+			i++;
+			if(i%2==0) {
+				result -= i;
+			}else {
+				result += i;
+			}
+		}
+		System.out.println("i="+i+", result="+result);
 	}
 }
