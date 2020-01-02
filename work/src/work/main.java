23f24dfaa8ad3c6ba43dbaea699987a5b1f3bb54
ssabchobo <lch9502@naver.com>
2020-01-02 17:13:47 +0900

    class3

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -4,45 +4,20 @@ import java.util.*;
 
 public class main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		Random r = new Random();
-		
-		int low,high;
-		int number;
-		
-		while(true) {
-			int step = 0;
-			low = 0;
-			high = 99;
-			
-			number = r.nextInt(100);
-			
-			System.out.println("수를 결정하였습니다. 맞추어 보세요");
-			
-			while(true) {
-				int input;
-				System.out.println(low + "-" + high);
-				System.out.print(step+1+">>");
-				input = in.nextInt();
-				
-				if(input<number) {
-					System.out.println("더 높게");
-					low = input;
-				}
-				else if(input<number) {
-					System.out.println("더 낮게");
-					high = input;
-				}else {
-					System.out.println("맞았습니다.");
-					break;
-				}
-				step ++;
-			}
-			System.out.print("다시 하시겠습니까(y/n)>>");
-			String answer = in.next();
-			if(answer.contentEquals("n")) {
-				break;
-			}
-		}
+		foo("안녕", 1);
+		foo("안녕하세요", 1, 2);
+		foo("잘 있어");
+	}
+	
+	public static void foo(String a, int b) {
+		System.out.println(a+" "+b);
+	}
+	
+	public static void foo(String a, int b, int c) {
+		System.out.println(a+" "+b+" "+c);
+	}
+	
+	public static void foo(String a) {
+		System.out.println(a);
 	}
 }
