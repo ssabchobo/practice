e0f83fb0164113556ec6f7162127bb38d03f45a2
ssabchobo <lch9502@naver.com>
2020-01-02 18:44:41 +0900

    쉽게 배우는 자바 프로그래밍 5-4

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -3,21 +3,29 @@ package work;
 import java.util.*;
 
 public class main {
-	public static int sum(int a, int b, int c) {
-		return b+c;
-	}
-	
-	public static int sum(int a,int... arr) {
-		int sum =0;
-		for(int i : arr) {
-			sum += i;
-		}
-		return sum;
-	}
-
 	public static void main(String[] args) {
-		System.out.println(sum(1,2,3));
-		int arr[] = {2,3};
-		System.out.println(sum(1,arr));
+		Scanner in = new Scanner(System.in);
+		String result;
+		String a = "com";
+		String b = "java";
+		String c = "bye";
+		
+		while(true) {
+			System.out.println("URL을 입력하세요 : ");
+			result = in.next();
+			
+			if(result.equals("bye")) {
+				break;
+			}
+			
+			if(result.endsWith(a)) {
+				System.out.println(result+"은 '"+a+"' 으로 끝납니다.");
+			}
+			else if(result.endsWith(b)) {
+				System.out.println(result+"은 '"+b+"' 으로 끝납니다.");
+			}else {
+				continue;
+			}
+		}	
 	}
 }
