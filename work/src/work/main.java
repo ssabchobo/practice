60a39bf3b15ae62d73ef8190c3bc63a1e092e2f2
ssabchobo <lch9502@naver.com>
2020-01-07 16:21:32 +0900

    상속

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,36 +2,37 @@ package work;
 
 import java.util.*;
 
-class ArrayUtil {
-	public static int[] concat(int[] a, int[] b) {
-		/* a와 b를 연결한 새로운 배열 리턴 */
-		int arr[] = new int[a.length+b.length];
-		
-		for(int i=0;i<a.length;i++) {
-			arr[i] = a[i];
-		}
-		
-		for(int i =0;i<b.length;i++) {
-			arr[a.length+i] = b[i];
-		}
-		return arr;
+class Point {
+	private int x,y;
+	void set(int x,int y) {
+		this.x=x;
+		this.y=y;
 	}
-
-	public static void print(int[] a) {
-		/* 배열 a 출력 */
-		System.out.print("[");
-		for(int n : a) {
-			System.out.print(n+" ");
-		}
-		System.out.print("]");
-		}
+	void showPoint() {
+		System.out.println("(" + x + "," + y + ")");
+	}
 }
 
-public class main {
-	public static void main (String[] args) {
-		int [] array1 = {1, 3, 5, 7, 9};
-		int [] array2 = {2, 4, 6, 8, 10};
-		int [] array3 = ArrayUtil.concat(array1, array2);
-		ArrayUtil.print(array3);
+class ColorPoint extends Point {
+	private String color;
+	void setColor(String color) {
+		this.color  = color;
+	}
+	void showColorPoint() {
+		System.out.print(color);
+		showPoint();
+	}
+}
+
+public class main{
+	public static void main(String[] args) {
+		Point p = new Point();
+		p.set(1, 2);
+		p.showPoint();
+		
+		ColorPoint cp = new ColorPoint();
+		cp.set(3, 4);
+		cp.setColor("red");
+		cp.showColorPoint();
 	}
 }
