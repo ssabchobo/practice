8253543dcde861261ffb876db389958385b84e10
ssabchobo <lch9502@naver.com>
2020-01-08 19:56:36 +0900

    foo,bar문제

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,73 +2,27 @@ package work;
 
 import java.util.*;
 
-class Point{
-	private int x,y;
-
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
+class Alpha{
+	public void foo(String a[]) {
+		System.out.println("Alpha: foo");
 	}
-
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	@Override
-	public String toString() {
-		return "Point [x=" + x + ", y=" + y + "]";
-	}	
-}
-
-class MovablePoint extends Point{
-	private int xSpeed,ySpeed;
-
-	public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
-		super(x, y);
-		this.xSpeed = xSpeed;
-		this.ySpeed = ySpeed;
-	}
-
-	public int getxSpeed() {
-		return xSpeed;
-	}
-
-	public void setxSpeed(int xSpeed) {
-		this.xSpeed = xSpeed;
-	}
-
-	public int getySpeed() {
-		return ySpeed;
-	}
-
-	public void setySpeed(int ySpeed) {
-		this.ySpeed = ySpeed;
-	}
-
-	@Override
-	public String toString() {
-		return "MovablePoint [xSpeed=" + xSpeed + ", ySpeed=" + ySpeed + "]";
+	public void bar(String a) {
+		System.out.println("Alpha: bar");
 	}
 }
-
-public class main{
-	public static void main(String[] args){
-		Point p1 = new Point(0,0);
-		MovablePoint p2 = new MovablePoint(0,0,0,0);
-		
-		System.out.println(p1);
-		System.out.println(p2);	
+public class main extends Alpha{
+	public void foo(String a) {
+		System.out.println("Beta: foo");
+	}
+	public void bar(String a) {
+		System.out.println("Beta: foo");
+	}
+	public static void main(String args[]) {
+		Alpha a = new main();
+		main b = (main)a;
+		//a.foo("test");
+		b.foo("test");
+		a.bar("test");
+		b.bar("test");
 	}
 }
