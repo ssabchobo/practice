5ec084306257dda37bebd8a5122c828f1c7f3993
ssabchobo <lch9502@naver.com>
2020-01-11 16:36:28 +0900

    쉽게 배우는 자바 프로그래밍 7.4

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,41 +2,29 @@ package work;
 
 import java.util.*;
 
-class Book implements Comparable<Book>{
-	int price;
-	
-	Book(int price){
-		this.price = price;
-	}
-	
-	public void sort() {
-		System.out.println("Book [price = " + price + "]");
-	}
-	
-	@Override
-    public int compareTo(Book book) {
-        return this.price - book.price;
-    }
+interface Talkable{
+	void talk();
 }
 
-public class main {
-	 
-    public static void main(String[] args) {
- 
-        Book[] books = new Book[] { new Book(15000), new Book(50000), new Book(20000) };
-        
-        System.out.println("정렬 전");
-        for (Book b1 : books) {
-            b1.sort();
-        }
-        
-        System.out.println();
-        Arrays.sort(books);
-        
-        System.out.println("정렬 후");
-        for (Book b2 : books) {
-            b2.sort();
-        }      
- 
-    } 
+class Korean implements Talkable{
+	public void talk() {
+		System.out.println("안녕하세요!");
+	}
 }
+
+class American implements Talkable{
+	public void talk() {
+		System.out.println("Hello!");
+	}
+}
+
+public class main{
+	static void speak(Talkable a) {
+		a.talk();
+	}
+	public static void main(String[] args) {
+		speak(new Korean());
+		speak(new American());
+	}
+}
+
