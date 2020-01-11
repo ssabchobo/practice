224307749846eb665687bce3836659d53744f9a9
ssabchobo <lch9502@naver.com>
2020-01-11 16:11:32 +0900

    쉽게 배우는 자바 7.3

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,31 +2,41 @@ package work;
 
 import java.util.*;
 
-abstract class Abstract{
-	int i;
+class Book implements Comparable<Book>{
+	int price;
 	
-	public Abstract(int i) {
-		this.i = i;
+	Book(int price){
+		this.price = price;
 	}
-
-	abstract void show();
+	
+	public void sort() {
+		System.out.println("Book [price = " + price + "]");
+	}
+	
+	@Override
+    public int compareTo(Book book) {
+        return this.price - book.price;
+    }
 }
 
-class Concrete extends Abstract{
-	int j;
-	
-	public Concrete(int i,int j) {
-		super(i);
-		this.j = j;
-	}
-	public void show() {
-		System.out.println("i = "+i+" j = "+j);
-	}
-}
-
-public class main{
-	public static void main(String[] args) {
-		Concrete c = new Concrete(100,50);
-		c.show();
-	}
+public class main {
+	 
+    public static void main(String[] args) {
+ 
+        Book[] books = new Book[] { new Book(15000), new Book(50000), new Book(20000) };
+        
+        System.out.println("정렬 전");
+        for (Book b1 : books) {
+            b1.sort();
+        }
+        
+        System.out.println();
+        Arrays.sort(books);
+        
+        System.out.println("정렬 후");
+        for (Book b2 : books) {
+            b2.sort();
+        }      
+ 
+    } 
 }
