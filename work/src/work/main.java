7442d712ab41adc0e9e4eb3ff484f230e7a04e4c
ssabchobo <lch9502@naver.com>
2020-01-12 14:31:56 +0900

    ppt 예제 9-11

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -4,14 +4,15 @@ import java.util.*;
 
 public class main{
 	public static void main(String[] args) {
-		int[] src = new int[] {1,2,3,4,5,6};
-		int[] dst = new int[] {100,200,300,400,500,600,700};
+		String s = "of the people, by the people, for the people";
 		
-		System.arraycopy(src,1,dst,3,3);
-		for(int i = 0; i<dst.length;i++) {
-			System.out.print(dst[i]+" ");
+		StringTokenizer st = new StringTokenizer(s," ,");
+		
+		System.out.println(st.countTokens());
+		
+		while(st.hasMoreTokens()) {
+			System.out.println(st.nextToken());
 		}
-		System.out.println();
 	}
 	
 }
