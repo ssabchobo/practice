df8bdebbfb66328096769375d5f0ca121b81eebd
ssabchobo <lch9502@naver.com>
2020-01-12 14:51:54 +0900

    실습과제 chap09-2

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -4,14 +4,26 @@ import java.util.*;
 
 public class main{
 	public static void main(String[] args) {
-		String s = "of the people, by the people, for the people";
+		Scanner in = new Scanner(System.in);
+		System.out.println("입력: ");
+		String s = in.nextLine();
 		
-		StringTokenizer st = new StringTokenizer(s," ,");
+		StringTokenizer st = new StringTokenizer(s," .");
 		
 		System.out.println(st.countTokens());
+		String[] arr = new String[st.countTokens()];
 		
+		int i=0;
 		while(st.hasMoreTokens()) {
-			System.out.println(st.nextToken());
+			arr[i] = st.nextToken();
+			i++;
+		}
+		Arrays.sort(arr);
+		System.out.println("정렬된 토큰: ");
+		int j=0;
+		while(j < arr.length) {
+			System.out.println(arr[j]);
+			j++;
 		}
 	}
 	
