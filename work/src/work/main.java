13f6f0ff8e364d9408e6521229509f54c42ea1ba
ssabchobo <lch9502@naver.com>
2020-01-13 19:49:36 +0900

    제네릭연습2

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,33 +2,47 @@ package work;
 
 import java.util.*;
 
+class GStack<T>{
+	int tos;
+	Object [] stck;
+	public GStack() {
+		tos =0;
+		stck = new Object [10];
+	}
+	public void push(T item) {
+		if(tos == 10) {
+			return;
+		}
+		stck[tos] = item;
+		tos ++;
+	}
+	public T pop() {
+		if(tos==0) {
+			return null;
+		}
+		tos--;
+		return (T)stck[tos];
+	}
+}
+
 public class main{
 	public static void main(String[] args) {
-		HashMap<String,String> dic = new HashMap<String, String>();
+		GStack<String> stringstack = new GStack<String>();
+		stringstack.push("Seoul");
+		stringstack.push("Busan");
+		stringstack.push("LA");
 		
-		dic.put("baby", "아기");
-		dic.put("love", "사랑");
-		dic.put("apple", "사과");
-		
-		Set<String> keys = dic.keySet();
-		Iterator<String> it = keys.iterator();
-		
-		while(it.hasNext()) {
-			String key = it.next();
-			String value = dic.get(key);
-			System.out.println("("+key+","+value+")");
+		for(int i = 0; i<3;i++) {
+			System.out.println(stringstack.pop());
 		}
-		System.out.println();
-		Scanner in = new Scanner(System.in);
-		for(int i=0;i<3;i++) {
-			System.out.println("찾고 싶은 단어?");
-			String eng = in.next();
-			String kor = dic.get(eng);
-			if(kor == null) {
-				System.out.println("없는 단어");
-			}else {
-				System.out.println(kor);
-			}
+		
+		GStack<Integer> Intstack = new GStack<Integer>();
+		Intstack.push(1);
+		Intstack.push(3);
+		Intstack.push(5);
+		
+		for(int i = 0; i<3;i++) {
+			System.out.println(Intstack.pop());
 		}
 	}
 }
