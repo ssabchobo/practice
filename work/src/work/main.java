c3a4d13f4379b56889d5bd93935fc34cb198a92b
ssabchobo <lch9502@naver.com>
2020-01-13 10:30:07 +0900

    연습

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,29 +2,26 @@ package work;
 
 import java.util.*;
 
-public class main{
+class A{
+	int i;
+}
+
+class B extends A{
+	int j;
+}
+
+class C extends B{
+	int k;
+}
+
+class D extends B{
+	int m;
+}
+
+public class main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("입력: ");
-		String s = in.nextLine();
-		
-		StringTokenizer st = new StringTokenizer(s," .");
-		
-		System.out.println(st.countTokens());
-		String[] arr = new String[st.countTokens()];
-		
-		int i=0;
-		while(st.hasMoreTokens()) {
-			arr[i] = st.nextToken();
-			i++;
-		}
-		Arrays.sort(arr);
-		System.out.println("정렬된 토큰: ");
-		int j=0;
-		while(j < arr.length) {
-			System.out.println(arr[j]);
-			j++;
-		}
+		A x = new D();
+		System.out.println(x instanceof B);
+		System.out.println(x instanceof C);
 	}
-	
 }
