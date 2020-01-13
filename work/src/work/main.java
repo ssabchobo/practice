3a4140ac59c96ed7f9627f25b9a1bd9dd665becd
ssabchobo <lch9502@naver.com>
2020-01-13 17:38:01 +0900

    실습과제06.2

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,80 +2,52 @@ package work;
 
 import java.util.*;
 
-class Car{
-	String color;
-	int speed;
-	
-	public Car(String color, int speed) {
-		this.color = color;
-		this.speed = speed;
+class Person{
+	String name;
+	int age;
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
 	}
-	
-	public String getColor() {
-		return color;
+	public void show() {
+		System.out.println("사람 ( 이름 = "+ name+", 나이 = "+age);
 	}
-
-	public int getSpeed() {
-		return speed;
-	}
-
-	public void upSpeed(int up){
-		if(speed<=300&&speed>=0) {
-			speed += up;
-		}else{
-			speed =300;
-		}
-	}
-    public void downSpeed(int down){
-    	if(speed<=300&&speed>=0) {
-    		speed -= down;
-    		if(speed<0) {
-    			speed=0;
-    		}
-		}else{
-			speed =0;
-		}	
-    }
 }
 
-class Sedan extends Car{
-	int seatNum;
+class Student extends Person{
+	int number;
 
-	public Sedan(String color, int speed, int seatNum) {
-		super(color, speed);
-		this.seatNum = seatNum;
+	public Student(String name, int age, int number) {
+		super(name, age);
+		this.number = number;
 	}
-
-	public int getSeatNum() {
-		return seatNum;
-	}	
+	public void show() {
+		System.out.println("사람 ( 이름 = "+ name+", 나이 = "+age+", 학번 = "+number);
+	}
 }
 
-class Truck extends Car{
-	int capacity;
+class ForeignStudent extends Student{
+	String nation;
 
-	public Truck(String color, int speed, int capacity) {
-		super(color, speed);
-		this.capacity = capacity;
-	}
-
-	public int getCapacity() {
-		return capacity;
+	public ForeignStudent(String name, int age, int number, String nation) {
+		super(name, age, number);
+		this.nation = nation;
 	}	
+	public void show() {
+		System.out.println("사람 ( 이름 = "+ name+", 나이 = "+age+", 학번 = "+number+", 국적 = "+nation);
+	}
 }
 
 public class main{
 	public static void main(String[] args){
-		Sedan sedan1 = new Sedan("레드",0,5);
-		Truck truck1 = new Truck("블루",0,50);
+		Person[] p = new Person[3];
 		
-		sedan1.upSpeed(150);
-		sedan1.downSpeed(200);
-		truck1.upSpeed(150);
-		truck1.upSpeed(200);
+		p[0] = new Person("길동이",22);
+		p[1] = new Student("황진이",23,100);
+		p[2] = new ForeignStudent("amy",24,200,"U.S.A");
 		
-		System.out.printf("승용차의 색상은 %s, 속도는 %dkm, 좌석수는 %d개 입니다.\n",sedan1.getColor(),sedan1.getSpeed(),sedan1.getSeatNum());
-		System.out.printf("트럭의 색상은 %s, 속도는 %dkm, 적재량은 %d개 입니다.\n",truck1.getColor(),truck1.getSpeed(),truck1.getCapacity());
-		
+		for(Person p1:p) {
+			p1.show();
+		}
 	}
 }
