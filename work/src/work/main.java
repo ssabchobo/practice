0e18250bc610fb90a5ac6551c7e9a9d34743ae4c
ssabchobo <lch9502@naver.com>
2020-01-14 16:08:35 +0900

    실습과제 chap7.3

--- work/src/work/main.java
+++ work/src/work/main.java
@@ -2,82 +2,37 @@ package work;
 
 import java.util.*;
 
-class Student{
-	private String name;
-	private String dept;
-	private String sNo;
-	
-	public Student(String name, String dept, String sNo) {
-		this.name = name;
-		this.dept = dept;
-		this.sNo = sNo;
-	}
+interface Printable{
+	void showPaper();
+}
 
-	public String getName() {
-		return name;
+class A4 implements Printable{
+	public void showPaper() {
+		System.out.println("A4");
 	}
+}
 
-	public void setName(String name) {
-		this.name = name;
+class B5 implements Printable{
+	public void showPaper() {
+		System.out.println("B5");
 	}
+}
 
-	public String getDept() {
-		return dept;
-	}
-
-	public void setDept(String dept) {
-		this.dept = dept;
-	}
-
-	public String getsNo() {
-		return sNo;
-	}
-
-	public void setsNo(String sNo) {
-		this.sNo = sNo;
-	}
-
-	@Override
-	public String toString() {
-		return "Person [name=" + name + ", dept=" + dept + ", sNo=" + sNo + "]";
-	}
+class Call{
+	public void invoke(Printable p){
+		p.showPaper();
+		}
 }
 
 public class main{
 	public static void main(String[] args) {
-		HashMap<String, Student> map = new HashMap<String, Student>();
-		Scanner in = new Scanner(System.in);
+		Printable[] printers = new Printable[2];
+		printers[0] =  new A4();
+		printers[1] =  new B5();
 		
-		String[] names = {"김철수", "김영희", "이돌쇠", "이막둥", "박멍돌"};
-		String[] depts = {"컴퓨터공학과", "컴퓨터공학과", "전자공학과", "전기공학과", "정보공학과"};
-		String[] stuNos = {"20180001", "20180002", "20180003", "20180004", "20180005"};
-		
-		String inputname,inputdept,inputstuNos;
-		
-		for(int i=0;i<5;i++) {
-			inputname = names[i];
-			inputdept = depts[i];
-			inputstuNos = stuNos[i];
-			Student newarr = new Student(inputname,inputdept,inputstuNos);
-			//System.out.println(newarr);
-			map.put(inputstuNos,newarr);
+		Call c = new Call();
+		for(Printable printer: printers) {
+			c.invoke(printer);
 		}
-
-		while (true) {
-			System.out.println("학생을 검색하려면 학번을, 종료하려면 q를 입력하세요: ");
-			String str1 = in.next();
-			if (str1.equals("q")) {
-				System.out.println("종료");
-				break;
-			} else {
-				Student str2 = map.get(str1);
-				if (str2 == null) {
-					System.out.println("해당학생없음");
-				} else {
-					System.out.println(str2);
-				}
-			}
-		}
-		
 	}
 }
